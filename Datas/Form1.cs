a19363b604b4ba4bbeebf15a0d57f0b8a48133f6
Marcelo Vieira <vieicelo@gmail.com>
2025-06-26 10:26:06 -0300

    Melhorias no código

--- Datas/Form1.cs
+++ Datas/Form1.cs
@@ -12,46 +12,45 @@ namespace Datas
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            limparForm();
+            LimparForm();
             dtpData.Value = DateTime.Today;
         }
 
         private void btnCalcula_Click(object sender, EventArgs e)
         {
-            addDias();
+            AddDias();
 
         }
 
         private void btnLimpar_Click(object sender, EventArgs e)
         {
-            limparForm();
+            LimparForm();
         }
 
-        private void limparForm()
+        private void LimparForm()
         {
-            //dtpData.Value = DateTime.Today;
             lblDataFinal.Text = string.Empty;
             txtDias.Text = string.Empty;
             txtDias.Focus();
         }
 
-        private void addDias()
+        private void AddDias()
         {
             try
             {
                 var dataInformada = dtpData.Value;
-                var dataCalculada = dataInformada.AddDays(int.Parse(txtDias.Text));
+                var dataCalculada = dataInformada.AddDays(Convert.ToInt32(txtDias.Text));
                 lblDataFinal.Text = dataCalculada.ToShortDateString();
             }
             catch (FormatException)
             {
                 MessageBox.Show("Digite somente números.", "Atenção!", MessageBoxButtons.OK);
-                limparForm();
+                LimparForm();
             }
             catch(Exception)
             {
                 MessageBox.Show("Erro ao calcular data. Verifique o valor digitado.", "Atenção!", MessageBoxButtons.OK);
-                limparForm();
+                LimparForm();
             }
 
         }
